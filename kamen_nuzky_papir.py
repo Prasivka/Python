df314798f878558d49937023a0f9125b7389c4c3
Petr Oliva <petr.oliva@hotmail.cz>
2021-12-03 16:31:30 +0100

    Update kamen_nuzky_papir.py

--- kamen_nuzky_papir.py
+++ kamen_nuzky_papir.py
@@ -6,5 +6,7 @@ def game():
     pocet_hracu = int(input("Kolik bude hráčů? (obvykle 2) "))
     volby = ['kámen', 'nůžky', 'papír']
     
+    vstup_hrac = int(input("Vyber volbu: (1-kámen,2-nůžky,3-papír)"))
+    vstup_comp = random.randint(1,3)
     
 # konec
