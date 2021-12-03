b6633b7443825ab45ef15e9296683d4507f136f8
Petr Oliva <petr.oliva@hotmail.cz>
2021-12-03 14:29:25 +0100

    Create kamen_nuzky_papir.py

---
+++ kamen_nuzky_papir.py
@@ -0,0 +1,9 @@
+#! python3
+# Hra Kámen nůžky papír
+import random
+
+def game():
+    pocet_hracu = int(input("Kolik bude hráčů? (obvykle 2) "))
+    volby = ['kámen', 'nůžky', 'papír']
+    
+    
